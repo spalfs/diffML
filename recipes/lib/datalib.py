6a94fe843ec11229ddbb90a96897e6982eb9f50b
Tom Barrett <tombarrett@siu.edu>
2017-03-27 09:48:26 -0500

    added parser for recipes

---
+++ recipes/lib/datalib.py
@@ -0,0 +1,24 @@
+
+def diffRecipes(data1, data2):
+    diffData = {}
+    diffData["data1missing"] = []
+    diffData["data2missing"] = []
+    for data1Screen in data1:
+        found = False
+        for data2Screen in data2:
+            if data1Screen['Variable'] == data2Screen['Variable']:
+                found = True
+        if not found:
+            diffData["data2missing"].append(data1Screen['Variable'])
+
+    for data2Screen in data2:
+        found = False
+        for data1Screen in data1:
+            if data1Screen['Variable'] == data2Screen['Variable']:
+                found = True
+        if not found:
+            diffData["data1missing"].append(data2Screen['Variable'])
+
+    return diffData
+
+
