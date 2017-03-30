e625064499bace983b87066d2a6368a88ea7c283
Tom Barrett <tombarrett@siu.edu>
2017-03-30 08:19:41 -0500

    -renaming and moved TreeItem to its own .py

---
+++ diffML/lib/TreeItem.py
@@ -0,0 +1,47 @@
+from lib.CONSTANTS import NONE
+
+class TreeItem(object):
+    def __init__(self, data, parent=None, depth=0):
+        self.parentItem = parent
+        self.itemData = data
+        self.childItems = []
+        self.depth = depth
+        self.colorState = NONE
+        self.changed = False
+
+    def appendChild(self, item):
+        self.childItems.append(item)
+
+    def child(self, row):
+        return self.childItems[row]
+
+    def childCount(self):
+        return len(self.childItems)
+
+    def columnCount(self):
+        return len(self.itemData)
+
+    def data(self, column):
+        try:
+            return self.itemData[column]
+        except IndexError:
+            return None
+
+    def setData(self, data, column):
+        self.changed = True
+        tmp = list(self.itemData)
+        tmp[column] = data
+        self.itemData = tuple(tmp)
+
+    def resetChanged(self):
+        self.changed = False
+
+    def parent(self):
+        return self.parentItem
+
+    def row(self):
+        if self.parentItem:
+            return self.parentItem.childItems.index(self)
+
+    def setColorState(self,state):
+        self.colorState = state
--- diffML/lib/TreeModel.py
+++ diffML/lib/TreeModel.py
@@ -4,52 +4,7 @@ import xml.etree.ElementTree as ET
 
 from lib.CONSTANTS import NONE, HIERARCHY, CHANGED
 from lib.xmllib import find
-
-class TreeItem(object):
-    def __init__(self, data, parent=None, depth=0):
-        self.parentItem = parent
-        self.itemData = data
-        self.childItems = []
-        self.depth = depth
-        self.colorState = NONE
-        self.changed = False
-
-    def appendChild(self, item):
-        self.childItems.append(item)
-
-    def child(self, row):
-        return self.childItems[row]
-
-    def childCount(self):
-        return len(self.childItems)
-
-    def columnCount(self):
-        return len(self.itemData)
-
-    def data(self, column):
-        try:
-            return self.itemData[column]
-        except IndexError:
-            return None
-
-    def setData(self, data, column):
-        self.changed = True
-        tmp = list(self.itemData)
-        tmp[column] = data
-        self.itemData = tuple(tmp)
-
-    def resetChanged(self):
-        self.changed = False
-
-    def parent(self):
-        return self.parentItem
-
-    def row(self):
-        if self.parentItem:
-            return self.parentItem.childItems.index(self)
-
-    def setColorState(self,state):
-        self.colorState = state
+from lib.TreeItem import TreeItem
 
 class TreeModel(QAbstractItemModel):
     def __init__(self, path, parent=None):
