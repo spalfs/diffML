17061a553bbd23a6ab6bf517d4bc9b7f4cfcf3d6
Tom Barrett <tombarrett@siu.edu>
2017-03-31 02:59:45 -0500

    -added right click menu to both views so they can collapse and expand all elements -moved open function closer to save -fixed coloring bug

--- diffML/lib/TreeView.py
+++ diffML/lib/TreeView.py
@@ -1,4 +1,8 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QTreeView
 
 class TreeView(QTreeView):
-    print("")
+    def __init__(self, view):
+        super(TreeView, self).__init__()
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(lambda : view.rightClickMenu(self))
