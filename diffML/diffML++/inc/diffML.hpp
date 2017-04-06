b9d859cdfb8db5ac1cf53937dd9a0df3e9fb4c50
Tom Barrett <tombarrett@siu.edu>
2017-04-06 08:33:34 -0500

    -added a basic c++ qt project if I end up using that instead of pyqt5

---
+++ diffML/diffML++/inc/diffML.hpp
@@ -0,0 +1,7 @@
+#ifndef diffml_h
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QPushButton>
+
+
+#endif
---
+++ diffML/diffML++/src/diffML.cpp
@@ -0,0 +1,12 @@
+#include "inc/diffML.hpp"
+
+int main(int argc, char **argv){
+
+        QApplication app(argc,argv);
+        
+        QPushButton button ("H i !");
+
+        button.show();
+
+        return app.exec();
+}
