498614eb8d9c628c773a9bea05c9605ba5cb6d79
Tom Barrett <tombarrett@siu.edu>
2017-04-06 09:23:30 -0500

    -got basic menubar

--- diffML/diffML++/inc/diffML.hpp
+++ diffML/diffML++/inc/diffML.hpp
@@ -2,15 +2,16 @@
 #define diffml_h
 
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QSplitter>
+#include <QtWidgets/QMenuBar>
 
 class diffML : public QMainWindow
 {
-
-
-
-
-
-
+        public:
+                diffML();
+        
+        private:
+                void initMenu();
 };
 
 #endif
--- diffML/diffML++/src/diffML.cpp
+++ diffML/diffML++/src/diffML.cpp
@@ -1,2 +1,15 @@
 #include "inc/diffML.hpp"
 
+diffML::diffML(){
+        QSplitter Splitter; 
+        setCentralWidget(&Splitter);
+
+        initMenu();
+}
+
+void diffML::initMenu(){
+        QMenu *file = menuBar()->addMenu("&File");
+        QMenu *edit = menuBar()->addMenu("&Edit");
+        QMenu *view = menuBar()->addMenu("&View");
+        QMenu *help = menuBar()->addMenu("&Help");
+}
